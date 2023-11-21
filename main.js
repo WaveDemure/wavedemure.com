c298a7db35f5d809ce8c46bd16443985620d7b6a
Wave Demure <144980384+WaveDemure@users.noreply.github.com>
2023-11-21 13:22:13 -0600

    8th night...

---
+++ main.js
@@ -0,0 +1,9 @@
+window.onload = function () {
+    document.title = "WaveDemure";
+
+    var tempPara = document.createElement("h1");
+    tempPara.innerHTML = "this website is the 8th night at freddys\nhar har har"
+    document.body.appendChild(tempPara)
+
+    
+}
